88b4ab28280a623d759b08fb347b0e37a7d676a2
luisfelip2005 <gomesluisfelipe09@gmail.com>
2026-02-18 19:36:36 -0300

    ajustando imports

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-import express from "express";
-import cors from "cors";
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 app.use(cors());
+app.use(userRoutes)
 
 const PORT = process.env.dev || 3333;
 
