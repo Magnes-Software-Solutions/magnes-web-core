b272c6e258b8a631267dce77b22fc3092a7f79fa
beatriz-augusto <beatriz.augusto@sptech.school>
2026-02-26 15:32:52 -0300

    criação do model de cadastro de empresa

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,1 +1,11 @@
-// code here
+var database = require("../database/config")
+
+function cadastrarEmpresar(nomeFabricante, cnpj, email, tel_celular, tel_corporativo) {
+    var instrucaoSql = `INSERT INTO fabricante (nomeFabricante, cnpj, email, tel_celular, tel_corporativo) 
+    VALUES ('${nomeFabricante}', '${cnpj}', '${email}', '${tel_celular}', '${tel_corporativo}')`
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    cadastrarEmpresa
+};
