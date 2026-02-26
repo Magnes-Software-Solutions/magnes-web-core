3883c8f68aff1aa878a050b19defec9367da62b5
lluan-iada <luan.nascimento@sptech.school>
2026-02-26 15:48:17 -0300

    Adjusting the route names (userRoutes)

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ require("dotenv").config();
 
 const app = express();
 
-const indexRouter = require("./src/routes/index")
+const userRoutes = require("./src/routes/userRoute")
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter)
+app.use("/user", userRoutes)
 
 const PORT = process.env.dev || 3333;
 
