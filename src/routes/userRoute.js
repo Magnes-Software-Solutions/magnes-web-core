f8ce8510f315c515a906f36d9d328567f7264d66
lluan-iada <luan.nascimento@sptech.school>
2026-02-27 15:31:59 -0300

    Merge branch 'cadastro-empresa/main'

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -8,4 +8,10 @@ routes.post("/autenticar", async (req, res) => {
     await user.autenticar(req, res)
 })
 
+routes.post("/cadastrarEmpresa", async (req, res) => {
+    const { name } = req.body
+    await user.cadastrarEmpresa(name)
+    res.send("User created")
+})
+
 module.exports = routes
