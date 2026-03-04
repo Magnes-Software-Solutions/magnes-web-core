164e29989e8ee07160e891296596421c00358212
luisfelip2005 <gomesluisfelipe09@gmail.com>
2026-03-03 22:03:06 -0300

    fixing userController error

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -14,12 +14,50 @@ async function autenticar(req, res){
 
             res.json (resultado)
         } else {
-            res.status(403).json("Email e/ou senha inválido(s)")
+            res.status(403).send("Email e/ou senha inválido(s)")
         }
     }
 
 }
 
+async function cadastrarEmpresa(req, res) {
+    var nomeFabricante = req.body.nomeFabricanteServer;
+    var cnpj = req.body.cnpjServer;
+    var email = req.body.emailFabricanteServer;
+    var tel_celular = req.body.tel_celularServer;
+    var tel_corporativo = req.body.tel_corporativoServer;
+
+    if (nomeFabricante == undefined) {
+        res.status(400).send("nome está undefined!");
+    } else if (cnpj == undefined) {
+        res.status(400).send("cnpj está undefined!");
+    } else if (email == undefined) {
+        res.status(400).send("email está undefined!");
+    } else if (tel_corporativo == undefined) {
+        res.status(400).send("tel_corporativo está undefined!");
+    } else {
+
+        userModel.cadastrarEmpresa(nomeFabricante, cnpj, email, tel_celular, tel_corporativo)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+
+    }
+
+}
+
 module.exports = {
-    autenticar
+    autenticar,
+    cadastrarEmpresa
 }
