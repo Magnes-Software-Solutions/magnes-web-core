6ba53f13f5996bec79f39c1a89b74689b9b8c904
lluan-iada <luan.nascimento@sptech.school>
2026-03-04 09:31:37 -0300

    Deleting duplicate file and adding employee role

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -14,12 +14,82 @@ async function autenticar(req, res){
 
             res.json (resultado)
         } else {
-            res.status(403).json("Email e/ou senha inválido(s)")
+            res.status(403).send("Email e/ou senha inválido(s)")
         }
     }
 
 }
 
+function cadastrarEmpresa(req, res) {
+    var nomeFabricante = req.body.nomeFabricanteServer;
+    var cnpj = req.body.cnpjServer;
+    var email = req.body.emailFabricanteServer;
+    var tel_celular = req.body.tel_celularServer;
+    var tel_corporativo = req.body.tel_corporativoServer;
+
+    if (nomeFabricante == undefined) {
+        res.status(400).send("nome está undefined!");
+    } else if (cnpj == undefined) {
+        res.status(400).send("cnpj está undefined!");
+    } else if (email == undefined) {
+        res.status(400).send("email está undefined!");
+    } else if (tel_corporativo == undefined) {
+        res.status(400).send("tel_corporativo está undefined!");
+    } else {
+
+        usuarioModel.cadastrarEmpresa(nomeFabricante, cnpj, email, tel_celular, tel_corporativo)
+            .then(
+                function (resultado) {
+                    console.log("Empresa cadastrada com sucesso!");
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+
+    }
+
+}
+
+function cadastrarFuncionario(req, res) {
+    const nome = req.body.nomeServer;
+    const email = req.body.emailServer;
+    const token = req.body.tokenServer;
+    const senha = req.body.senhaServer;
+
+    if (nome == undefined) {
+        res.status(400).send("nome está undefined!");
+    } else if (email == undefined) {
+        res.status(400).send("email está undefined!");
+    } else if (token == undefined) {
+        res.status(400).send("token está undefined!");
+    } else if (senha == undefined) {
+        res.status(400).send("senha está undefined!");
+    } else {
+        usuarioModel.cadastrarFuncionario(nome, email, token, senha)
+            .then(
+                function (resultado) {
+                    console.log("Funcionário cadastrado com sucesso!");
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
 module.exports = {
-    autenticar
+    autenticar,
+    cadastrarEmpresa,
+    cadastrarFuncionario
 }
