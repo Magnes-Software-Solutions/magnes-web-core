5ef7905c117fa462e93abe4b4901f23d585ba941
luisfelip2005 <gomesluisfelipe09@gmail.com>
2026-03-05 15:24:14 -0300

    fixing login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,10 +3,10 @@ const usuarioModel = require("../models/userModel")
 async function autenticar(req, res){
     const {email, senha} = req.body
 
-    if (email == undefined){
-        res.status(400).send("Seu email está undefined")
-    } else if (senha == undefined){
-        res.status(400).send("Sua senha está incorreta")
+    if (!email){
+        res.status(400).json("Seu email está undefined")
+    } else if (!senha){
+        res.status(400).json("Sua senha está incorreta")
     } else {
         const resultado = await usuarioModel.autenticar(email, senha)
 
@@ -14,7 +14,7 @@ async function autenticar(req, res){
 
             res.json (resultado)
         } else {
-            res.status(403).send("Email e/ou senha inválido(s)")
+            res.status(403).json("Email e/ou senha inválido(s)")
         }
     }
 
