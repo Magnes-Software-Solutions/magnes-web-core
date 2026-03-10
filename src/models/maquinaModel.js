9ac34ce42a3c68759638774ee277fff8fef27e26
luisfelip2005 <gomesluisfelipe09@gmail.com>
2026-03-10 15:52:02 -0300

    registering machines on database

--- src/models/maquinaModel.js
+++ src/models/maquinaModel.js
@@ -1,8 +1,8 @@
 const db = require("../database/config")
 
-async function adicionarMaquina(num_serie, modelo, fkFabricante) {
-    const instrucaoSql = `INSERT INTO maquina (numero_serie, modelo, fkFabricante, fkLocalizacao) VALUES
-    ("${num_serie}", "${modelo}", ${fkFabricante}, 1)`
+async function adicionarMaquina(num_serie, modelo, fkFabricante, fkLocalizacao) {
+    const instrucaoSql = `INSERT INTO maquina (numero_serie, modelo, fkFabricante, fkLocalizacao, dt_instalacao) VALUES
+    ('${num_serie}', '${modelo}', '${fkFabricante}', '${fkLocalizacao}', NOW())`
     
     await db.executar(instrucaoSql)
 }
