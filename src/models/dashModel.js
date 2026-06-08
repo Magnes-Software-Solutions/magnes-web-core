78662c1421dbe1685cf34af2b45b5b77f319d7c9
Caio18Godinho <caio.mota@sptech.school>
2026-06-08 18:21:30 -0300

    Create dashUsuario governanca

--- src/models/dashModel.js
+++ src/models/dashModel.js
@@ -1,7 +1,7 @@
 const database = require('../database/config.js')
 
 function pegarDashboard(idUsuario) {
-    var instrucaoSql = `SELECT nome, email, telefone, fkSupervisor FROM usuario WHERE idUsuario = ${idUsuario}`;
+    var instrucaoSql = `SELECT nome, email, telefone, fkSupervisor, supervisorFinanceiro FROM usuario WHERE idUsuario = ${idUsuario}`;
     return database.executar(instrucaoSql);
 }
 
